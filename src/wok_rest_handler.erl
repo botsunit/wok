7573d5d6e1726ed64f5d5c379c3bcc63d827d3d5
glejeune <gregoire.lejeune@free.fr>
2015-06-24 15:43:40 +0200

    Add http OPTIONS support

--- src/wok_rest_handler.erl
+++ src/wok_rest_handler.erl
@@ -7,18 +7,26 @@ routes(Routes) ->
   cowboy_router:compile([{'_', routes(Routes, [])}]).
 
 init(Req, Opts) ->
-  Method = cowboy_req:method(Req),
-  Action = list_to_atom(string:to_upper(binary_to_list(Method))),
-  {Code, Headers, Body} = try
-                            case lists:keyfind(Action, 1, Opts) of
-                              {Action, {Module, Function}} ->
-                                erlang:apply(Module, Function, [Req]);
-                              false ->
-                                {404, [], <<>>}
-                            end
-                          catch
-                            _:_ ->
-                              {500, [], <<>>}
+  Path = cowboy_req:path(Req),
+  lager:info("~p", [Path]),
+  {Code, Headers, Body} = case list_to_atom(
+                                 string:to_upper(
+                                   binary_to_list(
+                                     cowboy_req:method(Req)))) of
+                            'OPTIONS' ->
+                              {200, [{<<"Allow">>, allow(Path)}], <<>>};
+                            Action ->
+                              try
+                                case lists:keyfind(Action, 1, Opts) of
+                                  {Action, {Module, Function}} ->
+                                    erlang:apply(Module, Function, [Req]);
+                                  false ->
+                                    {404, [], <<>>}
+                                end
+                              catch
+                                _:_ ->
+                                  {500, [], <<>>}
+                              end
                           end,
   {ok, cowboy_req:reply(Code, Headers, Body, Req), Opts}.
 
@@ -43,3 +51,21 @@ compile(Routes) ->
   lists:map(fun({Path, Opts}) ->
                 {Path, ?MODULE, Opts}
             end, Routes).
+
+allow(Ressource) ->
+  list_to_binary(
+    string:join(
+      lists:foldl(
+        fun({Verb, Path, _}, Acc) ->
+            case {eutils:to_binary(Ressource),
+                  eutils:to_binary(Path)} of
+              {R, P} when R == <<"*">> orelse R == P -> 
+                case lists:member(atom_to_list(Verb), Path) of
+                  true -> Acc;
+                  false -> [atom_to_list(Verb)|Acc]
+                end;
+              _ -> 
+                Acc
+            end
+        end, ["OPTIONS"], wok_config:conf([wok, rest, routes], [])), ",")).
+
