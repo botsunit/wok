b2de1aebb3d076a4b03e1e0bfcf7924d71533ec4
glejeune <gregoire.lejeune@free.fr>
2016-02-29 17:59:20 +0100

    Update Elixir modules

--- lib/Wok.Request.ex
+++ lib/Wok.Request.ex
@@ -3,43 +3,61 @@
 # Using mix.mk (https://github.com/botsunit/mix.mk)
 # MODIFY IT AT YOUR OWN RISK AND ONLY IF YOU KNOW WHAT YOU ARE DOING!
 defmodule Wok.Request do
-  def unquote(:"custom_data")(arg1) do
-    :erlang.apply(:"wok_request", :"custom_data", [arg1])
-  end
-  def unquote(:"custom_data")(arg1, arg2) do
-    :erlang.apply(:"wok_request", :"custom_data", [arg1, arg2])
-  end
-  def unquote(:"client_ip")(arg1) do
-    :erlang.apply(:"wok_request", :"client_ip", [arg1])
-  end
-  def unquote(:"client_port")(arg1) do
-    :erlang.apply(:"wok_request", :"client_port", [arg1])
-  end
-  def unquote(:"body")(arg1) do
-    :erlang.apply(:"wok_request", :"body", [arg1])
-  end
-  def unquote(:"param")(arg1, arg2, arg3) do
-    :erlang.apply(:"wok_request", :"param", [arg1, arg2, arg3])
-  end
-  def unquote(:"param")(arg1, arg2) do
-    :erlang.apply(:"wok_request", :"param", [arg1, arg2])
-  end
-  def unquote(:"params")(arg1, arg2) do
-    :erlang.apply(:"wok_request", :"params", [arg1, arg2])
-  end
-  def unquote(:"params")(arg1) do
-    :erlang.apply(:"wok_request", :"params", [arg1])
-  end
-  def unquote(:"header")(arg1, arg2) do
-    :erlang.apply(:"wok_request", :"header", [arg1, arg2])
-  end
-  def unquote(:"header")(arg1, arg2, arg3) do
-    :erlang.apply(:"wok_request", :"header", [arg1, arg2, arg3])
-  end
-  def unquote(:"cookies")(arg1) do
-    :erlang.apply(:"wok_request", :"cookies", [arg1])
-  end
-  def unquote(:"cookie")(arg1, arg2) do
-    :erlang.apply(:"wok_request", :"cookie", [arg1, arg2])
-  end
+	def unquote(:"custom_data")(arg1) do
+		:erlang.apply(:"wok_request", :"custom_data", [arg1])
+	end
+	def unquote(:"custom_data")(arg1, arg2) do
+		:erlang.apply(:"wok_request", :"custom_data", [arg1, arg2])
+	end
+	def unquote(:"client_ip")(arg1) do
+		:erlang.apply(:"wok_request", :"client_ip", [arg1])
+	end
+	def unquote(:"client_port")(arg1) do
+		:erlang.apply(:"wok_request", :"client_port", [arg1])
+	end
+	def unquote(:"body")(arg1) do
+		:erlang.apply(:"wok_request", :"body", [arg1])
+	end
+	def unquote(:"method")(arg1) do
+		:erlang.apply(:"wok_request", :"method", [arg1])
+	end
+	def unquote(:"param")(arg1, arg2, arg3) do
+		:erlang.apply(:"wok_request", :"param", [arg1, arg2, arg3])
+	end
+	def unquote(:"param")(arg1, arg2) do
+		:erlang.apply(:"wok_request", :"param", [arg1, arg2])
+	end
+	def unquote(:"params")(arg1, arg2) do
+		:erlang.apply(:"wok_request", :"params", [arg1, arg2])
+	end
+	def unquote(:"params")(arg1) do
+		:erlang.apply(:"wok_request", :"params", [arg1])
+	end
+	def unquote(:"path")(arg1) do
+		:erlang.apply(:"wok_request", :"path", [arg1])
+	end
+	def unquote(:"header")(arg1, arg2) do
+		:erlang.apply(:"wok_request", :"header", [arg1, arg2])
+	end
+	def unquote(:"header")(arg1, arg2, arg3) do
+		:erlang.apply(:"wok_request", :"header", [arg1, arg2, arg3])
+	end
+	def unquote(:"cookies")(arg1) do
+		:erlang.apply(:"wok_request", :"cookies", [arg1])
+	end
+	def unquote(:"cookie")(arg1, arg2) do
+		:erlang.apply(:"wok_request", :"cookie", [arg1, arg2])
+	end
+	def unquote(:"local_state")(arg1) do
+		:erlang.apply(:"wok_request", :"local_state", [arg1])
+	end
+	def unquote(:"local_state")(arg1, arg2) do
+		:erlang.apply(:"wok_request", :"local_state", [arg1, arg2])
+	end
+	def unquote(:"global_state")(arg1) do
+		:erlang.apply(:"wok_request", :"global_state", [arg1])
+	end
+	def unquote(:"global_state")(arg1, arg2) do
+		:erlang.apply(:"wok_request", :"global_state", [arg1, arg2])
+	end
 end
--- lib/Wok.Response.ex
+++ lib/Wok.Response.ex
@@ -3,31 +3,37 @@
 # Using mix.mk (https://github.com/botsunit/mix.mk)
 # MODIFY IT AT YOUR OWN RISK AND ONLY IF YOU KNOW WHAT YOU ARE DOING!
 defmodule Wok.Response do
-  def unquote(:"render")(arg1, arg2) do
-    :erlang.apply(:"wok_response", :"render", [arg1, arg2])
-  end
-  def unquote(:"render")(arg1, arg2, arg3) do
-    :erlang.apply(:"wok_response", :"render", [arg1, arg2, arg3])
-  end
-  def unquote(:"render")(arg1, arg2, arg3, arg4) do
-    :erlang.apply(:"wok_response", :"render", [arg1, arg2, arg3, arg4])
-  end
-  def unquote(:"render")(arg1, arg2, arg3, arg4, arg5) do
-    :erlang.apply(:"wok_response", :"render", [arg1, arg2, arg3, arg4, arg5])
-  end
-  def unquote(:"redirect")(arg1, arg2) do
-    :erlang.apply(:"wok_response", :"redirect", [arg1, arg2])
-  end
-  def unquote(:"set_cookie")(arg1, arg2, arg3) do
-    :erlang.apply(:"wok_response", :"set_cookie", [arg1, arg2, arg3])
-  end
-  def unquote(:"set_cookie")(arg1, arg2, arg3, arg4) do
-    :erlang.apply(:"wok_response", :"set_cookie", [arg1, arg2, arg3, arg4])
-  end
-  def unquote(:"delete_cookie")(arg1, arg2) do
-    :erlang.apply(:"wok_response", :"delete_cookie", [arg1, arg2])
-  end
-  def unquote(:"set_response")(arg1, arg2) do
-    :erlang.apply(:"wok_response", :"set_response", [arg1, arg2])
-  end
+	def unquote(:"render")(arg1, arg2) do
+		:erlang.apply(:"wok_response", :"render", [arg1, arg2])
+	end
+	def unquote(:"render")(arg1, arg2, arg3) do
+		:erlang.apply(:"wok_response", :"render", [arg1, arg2, arg3])
+	end
+	def unquote(:"render")(arg1, arg2, arg3, arg4) do
+		:erlang.apply(:"wok_response", :"render", [arg1, arg2, arg3, arg4])
+	end
+	def unquote(:"render")(arg1, arg2, arg3, arg4, arg5) do
+		:erlang.apply(:"wok_response", :"render", [arg1, arg2, arg3, arg4, arg5])
+	end
+	def unquote(:"redirect")(arg1, arg2) do
+		:erlang.apply(:"wok_response", :"redirect", [arg1, arg2])
+	end
+	def unquote(:"set_cookie")(arg1, arg2, arg3) do
+		:erlang.apply(:"wok_response", :"set_cookie", [arg1, arg2, arg3])
+	end
+	def unquote(:"set_cookie")(arg1, arg2, arg3, arg4) do
+		:erlang.apply(:"wok_response", :"set_cookie", [arg1, arg2, arg3, arg4])
+	end
+	def unquote(:"delete_cookie")(arg1, arg2) do
+		:erlang.apply(:"wok_response", :"delete_cookie", [arg1, arg2])
+	end
+	def unquote(:"set_response")(arg1, arg2) do
+		:erlang.apply(:"wok_response", :"set_response", [arg1, arg2])
+	end
+	def unquote(:"set_headers")(arg1, arg2) do
+		:erlang.apply(:"wok_response", :"set_headers", [arg1, arg2])
+	end
+	def unquote(:"merge_headers")(arg1, arg2) do
+		:erlang.apply(:"wok_response", :"merge_headers", [arg1, arg2])
+	end
 end
