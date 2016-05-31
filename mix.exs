fe1a970d49d031d4e43a2d2f89f4875df2cc1724
glejeune <gregoire.lejeune@free.fr>
2016-05-31 17:39:27 +0200

    Bump to 0.3.0-rc.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Wok.Mixfile do
 
   def project do
     [app: :wok,
-     version: "0.3.0-rc.1",
+     version: "0.3.0-rc.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
