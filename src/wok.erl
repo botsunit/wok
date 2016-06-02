965bcf957427d7b241b39d0dc66cc31d1d32b77c
glejeune <gregoire.lejeune@free.fr>
2016-06-02 15:35:38 +0200

    Issue #32 : Remove deprecated Wok.provide/[2,4,5]

--- src/wok.erl
+++ src/wok.erl
@@ -3,44 +3,9 @@
 -export([start/0]).
 -export([state/0, state/1]).
 
--export([provide/5, provide/4, provide/2]).
-
--deprecated([{provide, 2, next_major_release},
-             {provide, 4, next_major_release},
-             {provide, 5, next_major_release}]).
-
 % @hidden
 start() ->
   application:ensure_all_started(?MODULE).
-
-% @doc
-% Send a message
-% @end
--spec provide(Topic :: binary() | list() | atom() | {binary() | list() | atom(), integer()},
-              From :: binary(),
-              To :: binary(),
-              Body :: term()) -> {ok, term()} | {error, term()}.
-provide(Topic, From, To, Body) ->
-  wok_producer:provide(Topic, From, To, Body).
-
-% @doc
-% Send a message
-% @end
--spec provide(Topic :: binary() | list() | atom() | {binary() | list() | atom(), integer()},
-              From :: binary(),
-              To :: binary(),
-              Body :: term(),
-              Options :: map()) -> {ok, term()} | {error, term()}.
-provide(Topic, From, To, Body, Options) ->
-  wok_producer:provide(Topic,  From, To, Body, Options).
-
-% @doc
-% Send a message
-% @end
--spec provide(Topic :: binary() | list() | atom() | {binary() | list() | atom(), integer()},
-              Message :: binary()) -> {ok, term()} | {error, term()}.
-provide(Topic, Message) ->
-  wok_producer:provide(Topic, Message).
 
 % @hidden
 -spec state() -> term().
