67f47f6493c161c61e64e825671ee25aad15cdcb
glejeune <gregoire.lejeune@free.fr>
2016-08-15 19:31:26 +0200

    msaas/wok#36 : call rest initializer

--- src/wok_app.erl
+++ src/wok_app.erl
@@ -15,33 +15,12 @@ stop(_State) ->
   ok.
 
 start_rest() ->
-  % TODO: IF wok_cowboy
-  % -->
-  % TODO: else
-  #{}.
-  % TODO: move in wok_cowboy
-  % case doteki:get_env([wok, rest]) of
-  %   undefined ->
-  %     lager:debug("No REST configuration."),
-  %     #{};
-  %   _ ->
-  %     _ = application:ensure_all_started(cowboy),
-  %     Port = doteki:get_env([wok, rest, port], ?DEFAULT_REST_PORT),
-  %     IP = bucinet:to_ip(doteki:get_env([wok, rest, ip], ?DEFAULT_REST_IP)),
-  %     TransOpts = [{port, Port}, {ip, IP}],
-  %     MaxConn = doteki:get_env([wok, rest, max_conn], ?DEFAULT_REST_MAX_CONN),
-  %     {Dispatch, Static} = wok_cowboy_handler:routes(),
-  %     ProtoOpts   = [{env, [{dispatch, Dispatch}]},
-  %                    {middlewares, [cowboy_router, cowboy_default_static_file, cowboy_handler]}],
-  %     case cowboy:start_http(http, MaxConn, TransOpts, ProtoOpts) of
-  %       {ok, _} ->
-  %         lager:info("Start HTTP on port ~p", [Port]),
-  %         Static;
-  %       _ ->
-  %         lager:error("Faild to start HTTP server"),
-  %         exit(http_error)
-  %     end
-  % end.
+  case bucs:function_exists(wok_rest_initializer, start, 0) of
+    true ->
+      wok_rest_initializer:start();
+    false ->
+      #{}
+  end.
 
 start_messages() ->
   case doteki:get_env([wok, messages]) of
