c5b6d2eb4629cbf942e2a69fd2dd1332171d3194
glejeune <gregoire.lejeune@free.fr>
2016-08-23 01:48:24 +0200

    Bump version to 0.5.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wok.Mixfile do
   def project do
     [
       app: :wok,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
