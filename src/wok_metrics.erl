8731d1184fb4b74e51d289169ff9ed808e6af116
glejeune <gregoire.lejeune@free.fr>
2016-10-06 09:27:27 +0200

    Ensure that the metrics app is started

--- src/wok_metrics.erl
+++ src/wok_metrics.erl
@@ -4,9 +4,23 @@
 -export([
          init_controler/2
          , controler_duration/3
+
+         , ensure_metrics_mod_started/0
         ]).
 
+ensure_metrics_mod_started() ->
+  ensure_metrics_mod_started(metrics:backend()).
+ensure_metrics_mod_started(metrics_folsom) ->
+  application:ensure_all_started(folsom);
+ensure_metrics_mod_started(metrics_exometer) ->
+  application:ensure_all_started(exometer);
+ensure_metrics_mod_started(metrics_grapherl) ->
+  application:ensure_all_started(grapherl);
+ensure_metrics_mod_started(_) ->
+  ok.
+
 init_controler(Module, Function) ->
+  ensure_metrics_mod_started(),
   metrics:new(gauge, controler_metric(Module, Function, <<"duration">>)).
 
 controler_duration(Module, Function, Duration) ->
