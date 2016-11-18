9d2d818c1873992e21fd00b22adeb86bd1ff3ff0
glejeune <gregoire.lejeune@free.fr>
2016-11-18 18:34:43 +0100

    Catch exception in async producer

--- src/wok_producer_async_srv.erl
+++ src/wok_producer_async_srv.erl
@@ -4,6 +4,9 @@
 -behaviour(gen_server).
 -include("../include/wok.hrl").
 -include("../include/wok_message_handler.hrl").
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
 
 %% API
 -export([start_link/1]).
@@ -66,12 +69,19 @@ handle_info(produce, #state{topic = Topic,
                             size = Size,
                             frequency = Frequency,
                             handler = Handler} = State) ->
-  Messages = erlang:apply(Handler, messages, [Topic, Partition, Size]),
-  case produce(Messages, Topic, Partition, Handler) of
-    true ->
-      {noreply, State#state{timer = erlang:send_after(Frequency, self(), produce)}};
-    false ->
-      {noreply, State#state{timer = undefined}}
+  try
+    Messages = erlang:apply(Handler, messages, [Topic, Partition, Size]),
+    case produce(Messages, Topic, Partition, Handler) of
+      true ->
+        {noreply, State#state{timer = erlang:send_after(Frequency, self(), produce)}};
+      false ->
+        {noreply, State#state{timer = undefined}}
+    end
+  catch
+    Class:Reason ->
+      lager:error("Async producer error: ~s",
+                  [lager:pr_stacktrace(erlang:get_stacktrace(), {Class, Reason})]),
+      {noreply, State#state{timer = erlang:send_after(Frequency, self(), produce)}}
   end;
 handle_info(_Info, State) ->
   {noreply, State}.
@@ -99,27 +109,29 @@ produce([{MessageID, Topic, Partition, Message, Retry} = M|Rest], Topic, Partiti
                     case wok_message:provide({Topic, Partition}, From, To, Body,
                                              [{headers, #{message_id => MessageID}}]) of
                       {ok, [#{partitions := [#{error_code := none}]}]} ->
-                        erlang:apply(Handler, response, [MessageID, ok, Retry]);
+                        call_handler(Handler, response, [MessageID, ok, Retry]);
                       {ok, [#{partitions := [#{error_code := KafeError}]}]} ->
-                        erlang:apply(Handler, response, [MessageID, {error, KafeError}, Retry]);
+                        call_handler(Handler, response, [MessageID, {error, KafeError}, Retry]);
                       E ->
-                        erlang:apply(Handler, response, [MessageID, E, Retry])
+                        call_handler(Handler, response, [MessageID, E, Retry])
                     end;
                   _ ->
-                    erlang:apply(Handler, response, [MessageID, {error, invalid_message}, Retry])
+                    call_handler(Handler, response, [MessageID, {error, invalid_message}, Retry])
                 end;
               {stop, Middleware, Reason} = Stop ->
                 lager:debug("Middleware ~p stop message ~p reason: ~p", [Middleware, Response1, Reason]),
-                erlang:apply(Handler, response, [MessageID, Stop, Reason]);
+                call_handler(Handler, response, [MessageID, Stop, Reason]);
               Other ->
                 lager:error("Invalid response for message ~p: ~p", [MessageID, Other]),
-                erlang:apply(Handler, response, [MessageID, {error, invalid_message}, Retry])
+                call_handler(Handler, response, [MessageID, {error, invalid_message}, Retry])
             end,
   provide_response(Result, M, Rest, Topic, Partition, Handler);
 produce([{MessageID, _, _, _, Retry} = M|Rest], Topic, Partition, Handler) ->
-  provide_response(erlang:apply(Handler, response, [MessageID, {error, wrong_topic_partition}, Retry]),
+  provide_response(call_handler(Handler, response, [MessageID, {error, wrong_topic_partition}, Retry]),
                    M, Rest, Topic, Partition, Handler).
 
+provide_response({error, _}, _, _, _, _, _) ->
+  true;
 provide_response(Response, {MessageID, Topic, Partition, Message, _}, Rest, Topic, Partition, Handler) ->
   case Response of
     retry ->
@@ -133,3 +145,114 @@ provide_response(Response, {MessageID, Topic, Partition, Message, _}, Rest, Topi
       lager:debug("Exit producer for ~p#~p", [Topic, Partition]),
       false
   end.
+
+call_handler(Handler, Function, Args) ->
+  try
+    erlang:apply(Handler, Function, Args)
+  catch
+    Class:Reason ->
+      lager:error("Async producer error: ~s",
+                  [lager:pr_stacktrace(erlang:get_stacktrace(), {Class, Reason})]),
+      {error, {Class, Reason}}
+  end.
+
+-ifdef(TEST).
+wok_producer_async_srv_test_() ->
+  {setup,
+   fun() ->
+       meck:new(test_async_handler, [non_strict]),
+       meck:expect(test_async_handler, response, 3, next),
+       meck:expect(test_async_handler, messages, 3,
+                   [
+                    {1, <<"topic">>, 0, base64:encode(term_to_binary(#wok_message{}))},
+                    {2, <<"topic">>, 0, base64:encode(term_to_binary(#wok_message{}))}
+                   ]),
+
+       meck:new(wok_middlewares),
+       meck:expect(wok_middlewares, outgoing_message, fun(R) -> {ok, R} end),
+
+       meck:new(wok_message, [passthrough]),
+       meck:expect(wok_message, provide, 5,
+                   {ok, [#{partitions => [#{error_code => none}]}]})
+
+   end,
+   fun(_) ->
+       meck:unload(wok_message),
+       meck:unload(wok_middlewares),
+       meck:unload(test_async_handler)
+   end,
+   [
+    fun() ->
+        ?assertMatch(
+           {noreply, #state{frequency = 10000,
+                            handler = test_async_handler,
+                            timer = _}},
+           handle_info(produce,
+                       #state{frequency = 10000,
+                              handler = test_async_handler}))
+    end,
+    fun() ->
+        ?assertMatch(
+           true,
+           produce([
+                    {1, <<"topic">>, 0, base64:encode(term_to_binary(#wok_message{}))},
+                    {2, <<"topic">>, 0, base64:encode(term_to_binary(#wok_message{}))}
+                   ], <<"topic">>, 0, test_async_handler))
+    end
+   ]}.
+
+wok_producer_async_srv_message_error_test_() ->
+  {setup,
+   fun() ->
+       meck:new(test_async_handler, [non_strict]),
+       meck:expect(test_async_handler, messages, fun(_, _, _) ->
+                                                     A = erlang:system_time(),
+                                                     timer:sleep(10),
+                                                     A = erlang:system_time()
+                                                 end)
+   end,
+   fun(_) ->
+       meck:unload(test_async_handler)
+   end,
+   [
+    fun() ->
+        ?assertMatch(
+           {noreply, #state{frequency = 10000,
+                            handler = test_async_handler,
+                            timer = _}},
+           handle_info(produce,
+                       #state{frequency = 10000,
+                              handler = test_async_handler}))
+    end
+   ]}.
+
+wok_producer_async_wok_provide_error_test_() ->
+  {setup,
+   fun() ->
+       meck:new(test_async_handler, [non_strict]),
+       meck:expect(test_async_handler, response, 3, next),
+
+       meck:new(wok_middlewares),
+       meck:expect(wok_middlewares, outgoing_message, fun(R) -> {ok, R} end),
+
+       meck:new(wok_message, [passthrough]),
+       meck:expect(wok_message, provide, 5,
+                   {ok, [#{partitions => [#{error_code => none}]}]})
+   end,
+   fun(_) ->
+       meck:unload(wok_message),
+       meck:unload(wok_middlewares),
+       meck:unload(test_async_handler)
+   end,
+   [
+    fun() ->
+        ?assertMatch(
+           true,
+           produce([
+                    {1, <<"topic">>, 0, base64:encode(term_to_binary(#wok_message{}))},
+                    {2, <<"topic">>, 0, base64:encode(term_to_binary(#wok_message{}))}
+                   ], <<"topic">>, 0, test_async_handler))
+    end
+   ]}.
+
+-endif.
