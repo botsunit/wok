3253a0f51351fa0f769ecffe275a969acac47a70
glejeune <gregoire.lejeune@free.fr>
2016-11-28 16:12:12 +0100

    Prepare 0.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wok.Mixfile do
   def project do
     [
       app: :wok,
-      version: "0.5.2",
+      version: "0.6.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -24,25 +24,31 @@ defmodule Wok.Mixfile do
   defp deps do
     [
       {:lager, "~> 3.2.0"},
-      {:lager_json_formatter, "~> 0.1.1"},
-      {:kafe, git: "https://github.com/botsunit/kafe.git", branch: "master"},
-      {:bucs, "~> 1.0.1"},
-      {:doteki, "~> 1.0.1"},
-      {:uuid, git: "https://github.com/botsunit/erlang-uuid.git", tag: "0.6.0"},
+      {:lager_json_formatter, "~> 0.2.0"},
+      {:kafe, "~> 2.1.0"},
+      {:bucs, "~> 1.0.2"},
+      {:doteki, "~> 1.0.2"},
+      {:uuid, git: "https://github.com/botsunit/erlang-uuid.git", tag: "0.7.0"},
       {:metrics, "~> 2.2.0"}    
     ]
   end
 
   defp aliases do
-    [compile: [&pre_compile_hooks/1, "compile", &post_compile_hooks/1]]
+    [compile: &compile_with_hooks/1]
   end
 
-  defp pre_compile_hooks(_) do
+  defp compile_with_hooks(args) do
+    pre_compile_hooks()
+    :ok = Mix.Task.run("compile", args)
+    post_compile_hooks()
+  end
+
+  defp pre_compile_hooks() do
     run_hook_cmd [
     ]
   end
 
-  defp post_compile_hooks(_) do
+  defp post_compile_hooks() do
     run_hook_cmd [
     ]
   end
