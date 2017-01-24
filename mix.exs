448a0c63a3314c5f4475e0ec4bc5e99eb37c61da
glejeune <gregoire.lejeune@free.fr>
2017-01-24 14:19:19 +0100

    Update doc

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wok.Mixfile do
   def project do
     [
       app: :wok,
-      version: "0.6.4",
+      version: "0.7.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
